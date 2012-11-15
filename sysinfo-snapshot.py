24ebe29401f5bbd6d7adb927080f8ccb7376d745
Luis De Siqueira <Luis@Mellanox.com>
2012-11-15 14:53:50 -0800

    Added discoverNetworkInterfaces and getNetworkInterfaces to the System class.  Providing a way to query for the network interfaces in a card.

--- sysinfo-snapshot.py
+++ sysinfo-snapshot.py
@@ -23,18 +23,8 @@ import struct
 import socket
 import fcntl
 
-
-
-# get_iface_list function definition
-# this function will return array of all 'up' interfaces
-
-
-# now, use the function to get the 'up' interfaces array
-ifaces = get_iface_list()
-
-# well, what to do? print it out maybe...
-for iface in ifaces:
-    print iface
+#non-standard libraries
+import netifaces
 
 class System:
     '''
@@ -55,28 +45,12 @@ class System:
         self.SIOCGIFCONF = 0x8912  #define SIOCGIFCONF
         self.BYTES = 4096          # Simply define the byte size
 
-        self.network_interfaces = self.discoverNetworkInterfaces()
+        self.network_interfaces = netifaces.interfaces()
         #OS environment variables in a dictionary
         self.env = os.environ
 
     def getNetworkInterfaces(self):
         return self.network_interfaces
-
-    def discoverNetworkInterfaces(self):
-        # create the socket object to get the interface list
-        sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        # prepare the struct variable
-        names = array.array('B', '\0' * self.BYTES)
-
-        # the trick is to get the list from ioctl
-        bytelen = struct.unpack('iL', fcntl.ioctl(sck.fileno(), self.SIOCGIFCONF, struct.pack('iL', self.BYTES, names.buffer_info()[0])))[0]
-
-        # convert it to string
-        namestr = names.tostring()
-
-        # return the interfaces as array
-        return [namestr[i:i+32].split('\0', 1)[0] for i in range(0, bytelen, 32)]
 
     def getSystem(self):
         return self.system
